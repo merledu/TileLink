2abe43eebae35c4dfd0a26b1b9e54f95116333df
Hadir Khan <hadirkhan10@gmail.com>
2020-07-30 22:28:46 +0500

    Created the TileLink Master (Host) adapter with initial configuration

---
+++ src/main/scala/tilelink/TL_HostAdapter.scala
@@ -0,0 +1,24 @@
+package tilelink
+import chisel3._
+
+class TL_HostAdapter(implicit val conf: TLConfiguration) extends Module {
+  val io = IO(new Bundle {
+    val req_i = Input(Bool())
+    val gnt_o = Output(Bool())
+    val addr_i = Input(UInt(conf.TL_AW.W))
+    val we_i = Input(Bool())
+    val wdata_i = Input(UInt(conf.TL_DW.W))
+    val be_i = Input(UInt(conf.TL_DBW.W))
+    val valid_o = Output(Bool())
+    val rdata_o = Output(UInt(conf.TL_DW.W))
+    val corr_o = Output(Bool())
+
+    val tl_o = new TL_H2D()
+    val tl_i = new TL_D2H()
+  })
+
+  when(reset.asBool()) {
+    io.tl_o.a_valid := false.B  // spec pg 11 during RESET the a_valid from master must be low
+  }
+
+}

7cd47eee985000e95309751971116b8536d1bd71
Hadir Khan <hadirkhan10@gmail.com>
2020-07-31 17:34:37 +0500

    Created a basic TL Host adapter

--- src/main/scala/tilelink/TL_HostAdapter.scala
+++ src/main/scala/tilelink/TL_HostAdapter.scala
@@ -1,5 +1,6 @@
 package tilelink
 import chisel3._
+import chisel3.util._
 
 class TL_HostAdapter(implicit val conf: TLConfiguration) extends Module {
   val io = IO(new Bundle {
@@ -20,5 +21,27 @@ class TL_HostAdapter(implicit val conf: TLConfiguration) extends Module {
   when(reset.asBool()) {
     io.tl_o.a_valid := false.B  // spec pg 11 during RESET the a_valid from master must be low
   }
+
+  val wordSize = log2Ceil(conf.TL_DBW)
+  val tl_source = Wire(UInt(conf.TL_AIW.W))
+  val tl_be = Wire(UInt(conf.TL_DBW.W))
+
+  tl_source := 0.U  // providing source identifier id => 0
+  tl_be := Mux(io.we_i, io.be_i, 1.U) // Using a mux to conditionally assign value to byte enable wire (tl_be)
+
+  io.tl_o.a_valid := io.req_i
+  io.tl_o.a_opcode := Mux(io.we_i, Mux((io.be_i & 1.U) === io.be_i, TL_A_Opcode.putFullData, TL_A_Opcode.putPartialData), TL_A_Opcode.get)
+  io.tl_o.a_param := 0.U
+  io.tl_o.a_size := wordSize.asUInt(conf.TL_SZW.W)
+  io.tl_o.a_mask := tl_be
+  io.tl_o.a_source := tl_source
+  io.tl_o.a_address := io.addr_i
+  io.tl_o.a_data := io.wdata_i
+  io.tl_o.d_ready := true.B
+
+  io.gnt_o := io.tl_i.a_ready
+  io.valid_o := io.tl_i.d_valid
+  io.rdata_o := io.tl_i.d_data
+  io.corr_o := io.tl_i.d_corrupt
 
 }

c601c6c8d7e80ed0ec0f3ffb729f76560475ee85
Hadir Khan <hadirkhan10@gmail.com>
2020-08-05 23:08:57 +0500

    Wiring for TileLink Register Adapter done with tl_err checking left

--- src/main/scala/tilelink/TL_RegAdapter.scala
+++ src/main/scala/tilelink/TL_RegAdapter.scala
@@ -33,8 +33,12 @@ class TL_RegAdapter(regAw: Int = 8, regDw: Int = 32)(regBw: Int = regDw/8)(impli
 
   val reqId = Wire(UInt(conf.TL_AIW.W))
   val reqSz = Wire(UInt(conf.TL_SZW.W))
+  val respOp = Wire(UInt(3.W))
+
   val rd_req = Wire(Bool())
   val wr_req = Wire(Bool())
+
+  val outstanding = Wire(Bool())
 
   a_ack := io.tl_i.a_valid && io.tl_o.a_ready
   d_ack := io.tl_o.d_valid && io.tl_i.d_ready
@@ -48,4 +52,45 @@ class TL_RegAdapter(regAw: Int = 8, regDw: Int = 32)(regBw: Int = regDw/8)(impli
   io.addr_o := Cat(io.tl_i.a_address(regAw-1, 2), 0.U(2.W)) // word aligned address
   io.wdata_o := io.tl_i.a_data
   io.be_o := io.tl_i.a_mask
+
+  when(!reset.asBool) {
+    outstanding := false.B
+    reqId := 0.U
+    reqSz := 0.U
+    respOp := TL_D_Opcode.accessAck
+    rdata := 0.U
+    error := false.B
+  } .elsewhen(a_ack) {
+    outstanding := true.B
+    reqId := io.tl_i.a_source
+    reqSz := io.tl_i.a_size
+    respOp := Mux(rd_req, TL_D_Opcode.accessAckData, TL_D_Opcode.accessAck)
+    rdata := Mux(err_internal, 1.U, io.rdata_i)
+    error := io.error_i || err_internal
+  } .elsewhen(d_ack) {
+    outstanding := false.B
+  } .otherwise {
+    outstanding := DontCare
+  }
+
+  io.tl_o.a_ready := !outstanding
+  io.tl_o.d_valid := outstanding
+  io.tl_o.d_opcode := respOp
+  io.tl_o.d_param := 0.U
+  io.tl_o.d_size := reqSz
+  io.tl_o.d_source := reqId
+  io.tl_o.d_sink := 0.U
+  io.tl_o.d_data := rdata
+  io.tl_o.d_error := error
+
+  err_internal := addr_align_err | tl_err
+
+  when(wr_req) {
+    addr_align_err := io.tl_i.a_address(1,0).orR
+  } .otherwise {
+    addr_align_err := false.B
+  }
+
+  // seperate tl_err checker which needs to be seen
+
 }

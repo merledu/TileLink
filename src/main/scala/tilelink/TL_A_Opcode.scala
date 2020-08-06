8cd4595f48d1022497c63fa5435df7c10527028d
Hadir Khan <hadirkhan10@gmail.com>
2020-08-07 00:26:34 +0500

    Added the width explicitly to all the opcodes which is 3 bits as defined in the spec

--- src/main/scala/tilelink/TL_A_Opcode.scala
+++ src/main/scala/tilelink/TL_A_Opcode.scala
@@ -1,7 +1,7 @@
 package tilelink
 import chisel3._
 object TL_A_Opcode {
-  val get = 4.U
-  val putFullData = 0.U
-  val putPartialData = 1.U
+  val get = 4.U(3.W)
+  val putFullData = 0.U(3.W)
+  val putPartialData = 1.U(3.W)
 }
--- src/main/scala/tilelink/TL_D_Opcode.scala
+++ src/main/scala/tilelink/TL_D_Opcode.scala
@@ -2,6 +2,6 @@ package tilelink
 import chisel3._
 
 object TL_D_Opcode {
-  val accessAck = 0.U
-  val accessAckData = 1.U
+  val accessAck = 0.U(3.W)
+  val accessAckData = 1.U(3.W)
 }

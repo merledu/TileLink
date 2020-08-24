05f3e0db50800023d23d36599b80cd6eb72313a6
Hadir Khan <hadirkhan10@gmail.com>
2020-08-25 00:11:39 +0500

    deleted unnecessary iotesters testbenches

--- src/test/scala/ArbiterUnitTest.scala
+++
@@ -1,6 +0,0 @@
-package tilelink
-import chisel3.iotesters._
-
-class ArbiterUnitTester(c:Arbiter) extends PeekPokeTester(c) {
-  step(10)
-}
--- src/test/scala/TLSocket1_NUnitTest.scala
+++
@@ -1,6 +0,0 @@
-package tilelink
-import chisel3.iotesters._
-
-class TLSocket1_NUnitTester(c: TLSocket1_N) extends PeekPokeTester(c) {
-  step(10)
-}

33ecac8e96c52340fe390738d6cd9dfbe7d96a69
Hadir Khan <hadirkhan10@gmail.com>
2020-08-26 22:13:26 +0500

    Created testbench for uart and gpio addressing

--- src/test/scala/TLDecoderTest.scala
+++ src/test/scala/TLDecoderTest.scala
@@ -7,10 +7,17 @@ class TLDecoderTest extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "TL Decoder"
   implicit val conf = TLConfiguration()
   val addrMap = AddressMap()
-  it should "just run" in {
-    test(new TL_Decoder(4, addrMap)) {c =>
+  it should "set the dev_sel to point to gpio" in {
+    test(new TL_Decoder(2, addrMap)) {c =>
       c.io.addr_i.poke("h40010000".U)
       c.io.dev_sel.expect(TL_Peripherals.deviceMap("gpio"))
+    }
+  }
+
+  it should "set the dev_sel to point to uart" in {
+    test(new TL_Decoder(2, addrMap)) {c =>
+      c.io.addr_i.poke("h40000000".U)
+      c.io.dev_sel.expect(TL_Peripherals.deviceMap("uart"))
     }
   }
 }

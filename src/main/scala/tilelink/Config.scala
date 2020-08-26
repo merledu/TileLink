bd262c01798a5ec45b2bd6322a1b7d2a88bdde79
hadirkhan10 <hadirkhan10@gmail.com>
2020-08-26 05:44:29 -0700

    Added an address map for gpio and device map for peripherals

--- src/main/scala/tilelink/Config.scala
+++ src/main/scala/tilelink/Config.scala
@@ -1,5 +1,7 @@
 package tilelink
+import chisel3._
 import chisel3.util._
+import scala.collection.immutable._
 
 case class TLConfiguration() {
   val TL_AW = 32                  // AW -> the default width of address bus
@@ -8,4 +10,13 @@ case class TLConfiguration() {
   val TL_DIW = 1                  // DIW -> Sink bits width
   val TL_DBW = (TL_DW >> 3)       // Number of data bytes generated (DW/8)
   val TL_SZW = log2Ceil(TL_DBW)   // The size width of operation in power of 2 represented in bytes
+}
+
+case class AddressMap() {
+  val ADDR_SPACE_GPIO = "h40010000".U
+  val ADDR_MASK_GPIO = "h00000fff".U
+}
+
+object TL_Peripherals {
+  val deviceMap: Map[String, UInt] = Map("gpio" -> 0.U, "uart" -> 1.U)
 }

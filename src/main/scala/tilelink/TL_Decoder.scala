ca22acf280bebfc5c7873a4a54c7f078eede31ea
Hadir Khan <hadirkhan10@gmail.com>
2020-08-26 22:10:38 +0500

    Added another condition in the decoder for the uart

--- src/main/scala/tilelink/TL_Decoder.scala
+++ src/main/scala/tilelink/TL_Decoder.scala
@@ -7,12 +7,10 @@ class TL_Decoder(N: Int, addrMap: AddressMap)(implicit val conf: TLConfiguration
     val dev_sel = Output(UInt(N.W))
   })
 
-  val gpio_mask = Wire(UInt(32.W))
-  gpio_mask := addrMap.ADDR_MASK_GPIO
-  when((io.addr_i & ~gpio_mask) === addrMap.ADDR_SPACE_GPIO) {
+  io.dev_sel := N.asUInt
+  when((io.addr_i & ~addrMap.ADDR_MASK_GPIO) === addrMap.ADDR_SPACE_GPIO) {
     io.dev_sel := TL_Peripherals.deviceMap("gpio")
-  } .otherwise {
-    io.dev_sel := 1.U
+  } .elsewhen((io.addr_i & ~addrMap.ADDR_MASK_UART) === addrMap.ADDR_SPACE_UART) {
+    io.dev_sel := TL_Peripherals.deviceMap("uart")
   }
-
 }
